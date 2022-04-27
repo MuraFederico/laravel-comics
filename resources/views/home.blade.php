cb450706c7e24efb3ede19bbf2aa419fddf129ad
MuraFederico <federicomura98@gmail.com>
2022-04-27 15:05:48 +0200

    raw header completed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('templates/main')
+
+@section('title', 'Home')
+
+@section('content')
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<header>
+    <div class="blue-band">
+        <div class="container">
+            <h4>DC POWER VISA</h4>
+            <h4>ADDITIONAL DC SITES</h4>
+        </div>
+    </div>
+    <nav>
+        <div class="container">
+            <div class="logo-container">
+                <img src="./images/dc-logo.png" alt="">
+            </div>
+
+            <ul class="nav-list">
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+
+            <div class="search">
+                <input type="text">
+            </div>
+        </div>
+
+        <div class="container-jumbo">
+            <img src="./images/jumbotron.jpg" alt="">
+        </div>
+    </nav>
+</header>
