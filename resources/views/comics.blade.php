448167d1a0311338ffce433cda2ad50d0c213848
MuraFederico <federicomura98@gmail.com>
2022-04-27 17:08:23 +0200

    terminal infinite refresh

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -24,4 +24,57 @@
         <button class="btn-expand">LOAD MORE</button>
     </div>
 </section>
+<section id="main-nav">
+    <nav class="nav-main">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="">
+                        <div class="container-img">
+                            <img src="images/buy-comics-digital-comics.png" alt="DIGITAL COMICS">
+                        </div>
+
+                        DIGITAL COMICS
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="container-img">
+                            <img src="images/buy-comics-merchandise.png" alt="DC MERCHANDISE">
+                        </div>
+
+                        DC MERCHANDISE
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="container-img">
+                            <img src="images/buy-comics-subscriptions.png" alt="SUBSCRIPTION">
+                        </div>
+
+                        SUBSCRIPTION
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="container-img">
+                            <img src="images/buy-comics-shop-locator.png" alt="COMIC SHOP LOCATOR">
+                        </div>
+
+                        COMIC SHOP LOCATOR
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="container-img">
+                            <img src="images/buy-dc-power-visa.svg" alt="DC POWER VISA">
+                        </div>
+
+                        DC POWER VISA
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</section>
 @endsection
